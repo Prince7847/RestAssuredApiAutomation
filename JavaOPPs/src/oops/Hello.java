56470573bea9e9300d9e6f313ce1a1a0c4690e41
Prince7847 <princedhaliya7847@gmail.com>
2025-09-04 12:11:31 +0530

    hello

---
+++ JavaOPPs/src/oops/Hello.java
@@ -0,0 +1,20 @@
+package oops;
+
+public class Hello {
+	
+	
+	
+	
+	private Hello() {
+		
+		System.out.println("constructor 123.");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Hello obj1 = new Hello();
+	}
+
+}
